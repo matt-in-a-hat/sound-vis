dd5081ef16bcd1168b4dcff78c6d722269852660
Matt Lang <matt@mediasuite.co.nz>
2014-01-03 00:12:10 +1300

    Hooked the piano up to the visualisation to show the wave of the note(s) selected.

--- main.js
+++ main.js
@@ -3,71 +3,6 @@
 
 $(function () {
 
-    var eq = function (x) {
-        var y = Math.sin(x);
-        return y;
-    };
-
-    var eq2 = function (x) {
-        var y = Math.sin(4 * x);
-        return y;
-    };
-
-    var periodScale = 4;
-
-
-    var visCanvasJQ = $('#visualisation_canvas');
-    var visCanvas = visCanvasJQ[0];
-    var visContext = visCanvas.getContext('2d');
-
-    visCanvas.height = visCanvasJQ.height();
-    visCanvas.width = visCanvasJQ.width();
-
-    var visCenterHeight = Math.ceil(visCanvas.height / 2);
-    var visCanvasWidth = visCanvas.width;
-
-    visContext.strokeStyle = "black";
-    visContext.lineWidth = 2;
-    visContext.beginPath();
-    visContext.moveTo(eq(1), visCenterHeight);
-
-    var x, y, scaledX;
-    var xScaler = visCanvasWidth / (periodScale * 2 * Math.PI);
-    for (x = 1; x < visCanvasWidth; x++) {
-        scaledX = x / xScaler;
-        y = visCenterHeight - ((eq(scaledX) + eq2(scaledX)) / 2) * visCenterHeight;
-        visContext.lineTo(x, y);
-    }
-    visContext.stroke();
-
-
-    // visContext.beginPath();
-    // visContext.moveTo(eq(1), visCenterHeight);
-    // visContext.strokeStyle = "blue";
-    // visContext.lineWidth = 1;
-
-    // xScaler = visCanvasWidth / (periodScale * 2 * Math.PI);
-    // for (x = 1; x < visCanvasWidth; x++) {
-    //     scaledX = x / xScaler;
-    //     y = visCenterHeight - eq(scaledX) * visCenterHeight;
-    //     visContext.lineTo(x, y);
-    // }
-    // visContext.stroke();
-
-
-    // visContext.beginPath();
-    // visContext.moveTo(eq(1), visCenterHeight);
-    // visContext.strokeStyle = "red";
-    // visContext.lineWidth = 1;
-
-    // xScaler = visCanvasWidth / (periodScale * 2 * Math.PI);
-    // for (x = 1; x < visCanvasWidth; x++) {
-    //     scaledX = x / xScaler;
-    //     y = visCenterHeight - eq2(scaledX) * visCenterHeight;
-    //     visContext.lineTo(x, y);
-    // }
-    // visContext.stroke();
-
 
     var PIANO_KEY_FREQUENCIES = {
         "C3": 130.81,
@@ -108,15 +43,105 @@ $(function () {
         "B5": 987.77
     };
 
+    var calculateY = function (x, key) {
+        var freq = PIANO_KEY_FREQUENCIES[key];
+        // The constant is just to scale it to fit on screen
+        // TODO define elsewhere (make zoomable?)
+        var y = Math.sin((x * freq) / 100);
+        return y;
+    };
+
+    var periodScale = 4;
+
+
+    var visCanvasJQ = $('#visualisation_canvas');
+    var visCanvas = visCanvasJQ[0];
+    var visContext = visCanvas.getContext('2d');
+
+    visCanvas.height = visCanvasJQ.height();
+    visCanvas.width = visCanvasJQ.width();
+
+    var visCenterHeight = Math.ceil(visCanvas.height / 2);
+    var visCanvasWidth = visCanvas.width;
+
+
     var playingKeys = {};
+
+    var updateVisualisation = function () {
+
+        visContext.fillStyle = "#EEE";
+        visContext.fillRect(0, 0, visCanvas.width, visCanvas.height);
+
+        visContext.strokeStyle = "black";
+        visContext.lineWidth = 2;
+        visContext.beginPath();
+
+        var x, y, scaledX, keys, noteTotal, noteCount, i;
+        var xScaler = visCanvasWidth / (periodScale * 2 * Math.PI);
+        var placed = false;
+        for (x = 1; x < visCanvasWidth; x++) {
+            scaledX = x / xScaler;
+
+            keys = Object.keys(playingKeys);
+            noteTotal = 0;
+            noteCount = keys.length;
+            for (i = keys.length - 1; i >= 0; i--) {
+                noteCount += 1;
+                noteTotal += calculateY(scaledX, keys[i]);
+            }
+
+            y = visCenterHeight - (noteTotal / noteCount) * visCenterHeight;
+            if (placed) {
+                visContext.lineTo(x, y);
+            } else {
+                visContext.moveTo(x, y);
+                placed = true;
+            }
+        }
+        visContext.stroke();
+    };
+
+
+    // visContext.beginPath();
+    // visContext.moveTo(eq(1), visCenterHeight);
+    // visContext.strokeStyle = "blue";
+    // visContext.lineWidth = 1;
+
+    // xScaler = visCanvasWidth / (periodScale * 2 * Math.PI);
+    // for (x = 1; x < visCanvasWidth; x++) {
+    //     scaledX = x / xScaler;
+    //     y = visCenterHeight - eq(scaledX) * visCenterHeight;
+    //     visContext.lineTo(x, y);
+    // }
+    // visContext.stroke();
+
+
+    // visContext.beginPath();
+    // visContext.moveTo(eq(1), visCenterHeight);
+    // visContext.strokeStyle = "red";
+    // visContext.lineWidth = 1;
+
+    // xScaler = visCanvasWidth / (periodScale * 2 * Math.PI);
+    // for (x = 1; x < visCanvasWidth; x++) {
+    //     scaledX = x / xScaler;
+    //     y = visCenterHeight - eq2(scaledX) * visCenterHeight;
+    //     visContext.lineTo(x, y);
+    // }
+    // visContext.stroke();
+
 
 
     $('.piano-key').on('click', function (e) {
         var key = e.currentTarget;
         var keyID = key.id;
         if (PIANO_KEY_FREQUENCIES[keyID]) {
-            playingKeys[keyID] = !playingKeys[keyID];
+            if (playingKeys[keyID]) {
+                delete playingKeys[keyID];
+            } else {
+                playingKeys[keyID] = true;
+            }
             $(key).toggleClass('playing-key');
+            updateVisualisation();
         }
     });
 
